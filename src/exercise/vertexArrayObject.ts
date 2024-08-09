2108bae4c56fcfebf76bc72de5a71e2c6739b9d3
Bhushan Wagh <bhushan@ctruh.com>
2024-08-09 13:27:04 +0530

    multiple draw calls with vertex array object

--- src/exercise/vertexArrayObject.ts
+++ src/exercise/vertexArrayObject.ts
@@ -31,6 +31,8 @@ gl.clearColor(1, 1, 0, 1);
 gl.clear(gl.COLOR_BUFFER_BIT);
 gl.clearDepth(1.0);
 
+//  1st triangle
+
 const program = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
 const positionData = new Float32Array([
@@ -44,6 +46,10 @@ const positionData = new Float32Array([
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, positionData, gl.STATIC_DRAW);
+
+const vao1 = gl.createVertexArray();
+gl.bindVertexArray(vao1);
+
 const positionAttribLocation = gl.getAttribLocation(program, "aPosition");
 
 gl.vertexAttribPointer(
@@ -66,7 +72,11 @@ const colorAttribLocation = gl.getAttribLocation(program, "aColor");
 gl.vertexAttribPointer(colorAttribLocation, 3, gl.FLOAT, false, 3 * 4, 0 * 4);
 gl.enableVertexAttribArray(colorAttribLocation);
 
+gl.bindVertexArray(null);
+
 gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+// 2nd triangle
 
 const program2 = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 const positionData2 = new Float32Array([
@@ -78,8 +88,13 @@ const positionData2 = new Float32Array([
 ]);
 
 const positionBuffer2 = gl.createBuffer();
+
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer2);
 gl.bufferData(gl.ARRAY_BUFFER, positionData2, gl.STATIC_DRAW);
+
+const vao2 = gl.createVertexArray();
+gl.bindVertexArray(vao2);
+
 const positionAttribLocation2 = gl.getAttribLocation(program2, "aPosition");
 
 gl.vertexAttribPointer(
@@ -102,6 +117,8 @@ const colorAttribLocation2 = gl.getAttribLocation(program, "aColor");
 gl.vertexAttribPointer(colorAttribLocation2, 3, gl.FLOAT, false, 3 * 4, 0 * 4);
 gl.enableVertexAttribArray(colorAttribLocation2);
 
+gl.bindVertexArray(null);
+
 gl.drawArrays(gl.TRIANGLES, 0, 3);
 
 let t = 0;
@@ -112,47 +129,49 @@ const animate = () => {
   gl.clearDepth(1.0);
 
   //1st triangle
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.vertexAttribPointer(
-    positionAttribLocation,
-    2,
-    gl.FLOAT,
-    false,
-    2 * 4,
-    0 * 4
-  );
-  gl.enableVertexAttribArray(positionAttribLocation);
+  //   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  //   gl.vertexAttribPointer(
+  //     positionAttribLocation,
+  //     2,
+  //     gl.FLOAT,
+  //     false,
+  //     2 * 4,
+  //     0 * 4
+  //   );
+  //   gl.enableVertexAttribArray(positionAttribLocation);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.vertexAttribPointer(colorAttribLocation, 3, gl.FLOAT, false, 3 * 4, 0 * 4);
-  gl.enableVertexAttribArray(colorAttribLocation);
+  //   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  //   gl.vertexAttribPointer(colorAttribLocation, 3, gl.FLOAT, false, 3 * 4, 0 * 4);
+  //   gl.enableVertexAttribArray(colorAttribLocation);
 
+  gl.bindVertexArray(vao1);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 
   //2nd triangle
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer2);
-  gl.vertexAttribPointer(
-    positionAttribLocation2,
-    2,
-    gl.FLOAT,
-    false,
-    2 * 4,
-    0 * 4
-  );
-  gl.enableVertexAttribArray(positionAttribLocation);
+  //   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer2);
+  //   gl.vertexAttribPointer(
+  //     positionAttribLocation2,
+  //     2,
+  //     gl.FLOAT,
+  //     false,
+  //     2 * 4,
+  //     0 * 4
+  //   );
+  //   gl.enableVertexAttribArray(positionAttribLocation);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer2);
-  gl.vertexAttribPointer(
-    colorAttribLocation2,
-    3,
-    gl.FLOAT,
-    false,
-    3 * 4,
-    0 * 4
-  );
-  gl.enableVertexAttribArray(colorAttribLocation2);
-
+  //   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer2);
+  //   gl.vertexAttribPointer(
+  //     colorAttribLocation2,
+  //     3,
+  //     gl.FLOAT,
+  //     false,
+  //     3 * 4,
+  //     0 * 4
+  //   );
+  //   gl.enableVertexAttribArray(colorAttribLocation2);
+  gl.bindVertexArray(vao2);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.bindVertexArray(null);
 
   requestAnimationFrame(animate);
   t++;
