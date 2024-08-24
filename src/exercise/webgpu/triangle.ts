d88449bbc4cb438cf85d029521cc2e9b2308cb31
Bhushan Wagh <bhushan@ctruh.com>
2024-08-24 21:49:03 +0530

    webgpu triangle

--- src/exercise/webgpu/triangle.ts
+++ src/exercise/webgpu/triangle.ts
@@ -1,6 +1,31 @@
 import "../../style.css";
 
+const shader = `
+struct VertexOutput {
+  @builtin(position) clip_position: vec4<f32>,
+};
+
+@vertex
+fn vs_main(
+  @builtin(vertex_index) in_vertex_index: u32,
+) -> VertexOutput {
+  var out: VertexOutput;
+  let x = f32(1 - i32(in_vertex_index)) * 0.5;
+  let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
+  out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
+  return out;
+}
+
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+  return vec4<f32>(0.0, 0.8, 0.1, 1.0);
+}
+`
+
 const canvas = document.getElementById("webglCanvas") as HTMLCanvasElement;
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
 if (!canvas) {
   throw new Error("Canvas not found");
@@ -50,9 +75,44 @@ async function webGPU() {
     colorAttachments: [colorAttachment],
   };
 
+  const shaderModule = device.createShaderModule({
+    code: shader
+  })
+
+  const layout = device.createPipelineLayout({
+    bindGroupLayouts: []
+  })
+
+  const colorState: GPUColorTargetState = {
+    format: "bgra8unorm"
+  }
+
+  const pipelineLayout: GPURenderPipelineDescriptor = {
+      layout,
+      vertex: {
+        module: shaderModule,
+        entryPoint: "vs_main",
+        buffers: []
+      },
+      fragment: {
+        module: shaderModule,
+        entryPoint: "fs_main",
+        targets: [colorState]
+      },
+      primitive: {
+        topology: 'triangle-list',
+        frontFace: "ccw",
+        cullMode: "back"
+      }
+  }
+
+  const pipeline = device.createRenderPipeline(pipelineLayout)
+
   const commandEncoder = device.createCommandEncoder();
   const passEncoder = commandEncoder.beginRenderPass(renderPassDesc);
   passEncoder.setViewport(0, 0, canvas.width, canvas.height, 0, 1);
+  passEncoder.setPipeline(pipeline)
+  passEncoder.draw(3, 1)
   passEncoder.end();
 
   device.queue.submit([commandEncoder.finish()]);
